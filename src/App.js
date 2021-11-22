8f3de46da07978d568ec1bc20de54ae39522053c
matusilva <matheus10xy@gmail.com>
2021-11-22 01:01:24 -0300

    criando app curriculo

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Alert, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import foto from '../assets/foto.png';
+
+export default function App() {
+
+  function handleSocialMedia(social_media) {
+    switch (social_media) {
+      case 'linkedin':
+        Alert.alert('Linkedin', 'https://linkedin.com/in/matusilva')
+        break
+      case 'github':
+        Alert.alert('Github', 'https://github.com/matusilva')
+        break
+    }
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={foto} style={styles.photo} />
+          <Text style={styles.name}>MATHEUS VINÍCIUS DA SILVA</Text>
+          <Text style={styles.description}>Desenvolvedor BACK-END</Text>
+          <View style={styles.social_medias}>
+            <TouchableOpacity onPress={() => handleSocialMedia('github')}>
+              <Ionicons name="logo-github" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleSocialMedia('linkedin')}>
+              <Ionicons name="logo-linkedin" size={30} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.card_container}>
+          <View style={styles.card}>
+            <View style={styles.card_header}>
+              <Text>Experiência Profissional</Text>
+            </View>
+            <View style={styles.card_content}>
+              <Text style={styles.card_content_text}>RITS TECNOLOGIA</Text>
+              <Text style={styles.card_content_text}>CONTROL</Text>
+              <Text style={styles.card_content_text}>PROCESSO ÁGIL</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.card_container}>
+          <View style={styles.card}>
+            <View style={styles.card_header}>
+              <Text>Formação Acadêmica</Text>
+            </View>
+            <View style={styles.card_content}>
+              <Text style={styles.card_content_text}>IFRN - Natal Central</Text>
+            </View>
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e7e7e7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  photo: {
+    width: 250,
+    height: 250,
+    borderRadius: 125
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  description: {
+    color: '#939393',
+    marginBottom: 10
+  },
+  social_medias: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  card_container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  card: {
+    width: '60%',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#939393',
+    padding: 10,
+    backgroundColor: '#fff'
+  },
+  card_content: {
+    marginTop: 10
+  },
+  card_content_text: {
+    color: '#939393',
+    marginBottom: 10
+  }
+});
